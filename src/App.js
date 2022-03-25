312627610bf4a9f27ddd0ce75d54403b2148704c
João Ramos <jdmor95@gmail.com>
2022-03-24 17:41:07 -0700

    adding assets, boilerplate code and cleaning files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+// Modules
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Routes
+import RouteLandingPage from './routes/RouteLanding';
+
+// App
 export default function App() {
   return (
-    <>
-      <h1>welcome.</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<RouteLandingPage />} />
+      </Routes>
+    </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+//style
+
+import './style/partials/_reset.scss';
+import './style/partials/_typography.scss';
 
 ReactDOM.render(
   <React.StrictMode>
