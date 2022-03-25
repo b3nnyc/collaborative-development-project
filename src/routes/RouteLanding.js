e95e35b5055042665d1ef3b8f7520056ae83df5c
Ben Cartwright <ben.cartwright@outlook.com>
2022-03-25 11:59:00 -0400

    finished mentorship landing page

--- src/routes/RouteLanding.js
+++ src/routes/RouteLanding.js
@@ -7,13 +7,14 @@ export default function RouteLading() {
       <h2 className="section__heading">Mentorship</h2>
       <div className="container">
         <img className="container__image" src={chair} alt="" />
-        <h3>Request or approve mentorship</h3>
-        <p>This is where you can become a mentor or find a mentor. You can also view mentors near you.</p>
+        <h3 className="container__header">Request or approve mentorship</h3>
+        <p className="container__copy">This is where you can become a mentor or find a mentor. You can also view mentors near you.</p>
         <div className="container__buttons">
           <button className="container__button1">Find mentor</button>
-          <button className="container__button1">Become mentor</button>
+          <button className="container__button2">Become mentor</button>
         </div>
       </div>
+      <p className="footer">Learn more about<a className="footer__link" href="http://google.com">mentorship</a></p>
     </section>
   );
 }
